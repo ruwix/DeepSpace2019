5f518dad81d759b76dbaec4489e3b26bbb80be27
ruwix <grantpauker@gmail.com>
2019-02-27 09:55:57 -0800

    Add new climb roller controls and tank arm button

--- src/oi.py
+++ src/oi.py
@@ -1,7 +1,7 @@
 from utils import joystick, singleton, intakestate, hatchlatchstate, gamestate
 from constants import Constants
 from wpilib.buttons.joystickbutton import JoystickButton
-from commands import sethatchlatchstate, drivetimed, setintakestate, setlongarm, setgamestate, rollclimbroller
+from commands import sethatchlatchstate, drivetimed, setintakestate, setlongarm, setgamestate, rollclimbroller, tankarm
 
 
 class OI(metaclass=singleton.Singleton):
@@ -42,6 +42,7 @@ class OI(metaclass=singleton.Singleton):
         self.climb_roll = rollclimbroller.RollClimbRoller(
             Constants.CLIMB_ROLLER_SPEED)
         self.climb_stop = rollclimbroller.RollClimbRoller(0)
+        self.tank_arm = tankarm.TankArm()
 
         self.scoot_left = drivetimed.DriveTimed(
             0, -Constants.SCOOT_SPEED, 0, Constants.SCOOT_DURATION)
@@ -51,7 +52,8 @@ class OI(metaclass=singleton.Singleton):
         self.operator_buttons[0].whenPressed(self.game_stow)
         self.operator_buttons[1].whenPressed(self.game_play)
         self.operator_buttons[2].whenPressed(self.game_start_climb)
-        self.operator_buttons[3].whenPressed(self.game_end_game)
+        self.operator_buttons[3].whenPressed(self.tank_arm)
+        
         # self.operator_buttons[4].whenPressed(self.intake_spit)
         # self.operator_buttons[4].whenReleased(self.intake_stop)
         # self.operator_buttons[5].whenPressed(self.intake_suck)
@@ -60,8 +62,11 @@ class OI(metaclass=singleton.Singleton):
         self.drive_buttons[4].whenPressed(self.scoot_left)
         self.drive_buttons[5].whenPressed(self.scoot_right)
 
-        self.operator_buttons[4].whenPressed(self.climb_roll)
-        self.operator_buttons[4].whenReleased(self.climb_stop)
+        self.operator_buttons[10].whenPressed(self.climb_roll)
+        self.operator_buttons[10].whenReleased(self.climb_stop)
+
+        self.operator_buttons[11].whenPressed(self.climb_roll)
+        self.operator_buttons[11].whenReleased(self.climb_stop)
 
         self.operator_buttons[6].whenPressed(self.hatch_close)
         self.operator_buttons[7].whenPressed(self.hatch_open)
