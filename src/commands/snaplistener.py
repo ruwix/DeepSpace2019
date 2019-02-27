ef1f8b25939ea43c13f7e7a5a6467e6fefad5171
ruwix <grantpauker@gmail.com>
2019-02-27 09:56:16 -0800

    Add is finished method

--- src/commands/snaplistener.py
+++ src/commands/snaplistener.py
@@ -7,6 +7,7 @@ from commands import turntoangle
 from subsystems import drive
 from utils import angles
 
+
 class SnapListener(Command):
     POV_ARRAY = [0, 28.75, 90, 151.25, -179.9, -151.25, -90, -28.75]
 
@@ -14,18 +15,23 @@ class SnapListener(Command):
         super().__init__()
         self.drive = drive.Drive()
         self.pov_port = 0
-    
+
     def initialize(self):
         pass
-    
+
     def execute(self):
         pov = oi.OI().driver.getPOV(self.pov_port)
-        #logging.debug("drive current command: {}".format(self.drive.currentCommand))
-        if pov != -1 and not isinstance(self.drive.currentCommand,turntoangle.TurnToAngle):
+        if pov != -1 and not isinstance(self.drive.currentCommand, turntoangle.TurnToAngle):
             pov = angles.positiveAngleToMixedAngle(pov)
-            pov /= 45
-            to_turn_to = SnapListener.POV_ARRAY[pov]
+            pov //= 45
+            to_turn_to = SnapListener.POV_ARRAY[int(pov)]
             to_turn_to = int(to_turn_to)
             Dash.putNumber("Snap Turning Target", to_turn_to)
-            logging.debug("Starting snap to angle")
+            # logging.debug("Starting snap to angle")
             turntoangle.TurnToAngle(to_turn_to).start()
+
+    def end(self):
+        pass
+
+    def isFinished(self):
+        return False
